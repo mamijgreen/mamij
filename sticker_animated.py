2b300451427ac7131fba8b76fbb140c214072945
mamijgreen <membbgreen@gmail.com>
2025-05-31 19:14:59 +0330

    Update sticker_animated.py

--- sticker_animated.py
+++ sticker_animated.py
@@ -1,5 +1,5 @@
 from PIL import Image
-import imageio
+import imageio.v3 as iio
 import numpy as np
 import random
 import os
@@ -15,31 +15,31 @@ class NSFWAnimatedStickerDetector:
         """
 
         try:
-            reader = imageio.get_reader(video_path)
-            total_frames = reader.count_frames()
-            if total_frames == 0:
-                return False
+            with iio.imopen(video_path, 'r') as file:
+                frames = list(file)
+                total_frames = len(frames)
+                if total_frames == 0:
+                    return False
 
-            sample_frames_count = min(5, total_frames)
-            frame_indices = random.sample(range(total_frames), sample_frames_count)
+                sample_frames_count = min(5, total_frames)
+                frame_indices = random.sample(range(total_frames), sample_frames_count)
 
-            skin_percentages = []
-            skin_tone = np.array([220, 180, 140])
+                skin_percentages = []
+                skin_tone = np.array([220, 180, 140])
 
-            for idx in frame_indices:
-                frame = reader.get_data(idx)
-                image_array = np.array(frame)
+                for idx in frame_indices:
+                    frame = frames[idx]
+                    image_array = np.array(frame)
 
-                # اگر تصویر RGBA بود، حذف کانال آلفا
-                if image_array.shape[2] == 4:
-                    image_array = image_array[:, :, :3]
+                    # اگر تصویر RGBA بود، حذف کانال آلفا
+                    if image_array.shape[2] == 4:
+                        image_array = image_array[:, :, :3]
 
-                skin_pixels = np.sum(np.all(np.abs(image_array - skin_tone) < 50, axis=-1))
-                skin_percentage = skin_pixels / (image_array.shape[0] * image_array.shape[1])
-                skin_percentages.append(skin_percentage)
+                    skin_pixels = np.sum(np.all(np.abs(image_array - skin_tone) < 50, axis=-1))
+                    skin_percentage = skin_pixels / (image_array.shape[0] * image_array.shape[1])
+                    skin_percentages.append(skin_percentage)
 
-            avg_skin_percentage = sum(skin_percentages) / len(skin_percentages)
-            reader.close()
+                avg_skin_percentage = sum(skin_percentages) / len(skin_percentages)
 
             return avg_skin_percentage > 0.3
 
